751faf624e8f4d908427760498bfbd7e0dce1b38
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-08-21 14:22:57 -0600

    Create project

---
+++ src/test/java/codes/ledesma/runnerz/RunnerzApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.ledesma.runnerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
