daf78f8245edab90ca644d382b25bd5bd5dc970d
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-08-26 23:52:07 -0600

    Add RunNotFoundException

--- src/main/java/codes/ledesma/runnerz/run/RunController.java
+++ src/main/java/codes/ledesma/runnerz/run/RunController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController // anotation: says that this class responds to requests
 @RequestMapping("/api/runs") // Every request mapping falls under this URL
@@ -38,8 +37,8 @@ public class RunController {
     public Run findById(@PathVariable Integer id) {
         Optional<Run> run = this.runRepository.getRunById(id);
         if (!run.isPresent()) {
-            // To sen error status use this exception
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            // Custom exception to send error status use
+            throw new RunNotFoundException();
         }
         return run.get();
     }
